b35b2e9b1f06ddf44155a68441d8695fd8a9b3f1
Hao Fang <haofang97@gmail.com>
2024-06-16 20:02:55 +0800

    set up simple route to article-details/:id

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react";
 import Home from "./components/Home/Home";
+import ArticleDetails from "./components/ArticleDetails/ArticleDetails";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/article-details/:id" element={<ArticleDetails />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/ArticleDetails/ArticleDetails.tsx
@@ -0,0 +1,9 @@
+function ArticleDetails() {
+  return (
+    <div>
+      <h1>Article Details</h1>
+    </div>
+  );
+}
+
+export default ArticleDetails;
